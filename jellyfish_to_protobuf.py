a4c87a4939057ea2129848b9359c80579989e959
grant dexter <grant@nav.com>
2019-10-04 11:18:04 -0600

    Adds exceptions and custom field typing

--- jellyfish_to_protobuf.py
+++ jellyfish_to_protobuf.py
@@ -6,11 +6,59 @@ def format_single(field_name):
   s1 = re.sub('([a-z])([0-9])', r'\1_\2', s1)
   return re.sub('([a-z])([A-Z])', r'\1_\2', s1).lower()
 
+def is_verified_string_field(message_name, field_name):
+  string_fields = [
+    "Initials.value",
+    "LocationDescription.value",
+    "LiabilityIndicator.value",
+    "OverallStatus.value",
+    "NumberOfEmployees.value",
+    "Title.value",
+    "CompanyOwnsership.value",
+    "CorpStatusCode.value",
+    "AccountComments.value",
+    "PaymentRating.value",
+    "PreviousAccountReference.value",
+    "AccountReference.value",
+    "ProductCode.value",
+    "Reject.value",
+    "ReasonCode.value",
+    "AnnualSalesRange.value",
+  ]
+  for sf in string_fields:
+    names = sf.split(".")
+    if message_name == names[0] and field_name == names[1]:
+      return True
+  return False
+
+def format_leaf(field_name, message_name):
+  field_name = format_single(field_name)
+
+  # make string default type
+  pb_type = "string"
+  
+  if is_verified_string_field(message_name, field_name):
+    return pb_type + " " + field_name
+
+  elif field_name == "value" or \
+    "num_" in field_name or \
+    field_name.startswith("count_") or \
+    field_name.startswith("years_") or \
+    field_name.startswith("number_"):
+
+    pb_type = "int64"
+  
+  return pb_type + " " + field_name
+
 def format_repeated(field_name):
-  fn = format_single(field_name)
-  if fn.endswith("s"):
-    return fn
-  return fn + "s"
+  field_name = format_single(field_name)
+  if field_name.endswith("s"):
+    return field_name
+  return field_name + "s"
+
+def format_repeated_leaf(field_name, message_name):
+  field_name = format_leaf(field_name, message_name)
+  return format_repeated(field_name)
 
 def read_jellyfish_output(filename):
   with open(filename) as f:
@@ -57,11 +105,11 @@ def write_top_level(top_level_objs, f):
       field = field.replace("\n", "")
 
     if repeated and leaf: # I don't think we have any of these yet
-      root_fields += "\n  repeated string {} = {};".format(format_repeated(field), idx + 5)
+      root_fields += "\n  repeated string {} = {};".format(format_repeated_leaf(field, "x"), idx + 5)
     elif repeated and not leaf:
       root_fields += "\n  repeated {} {} = {};".format(field, format_repeated(field), idx + 5)
     elif not repeated and leaf:
-      root_fields += "\n  string {} = {};".format(format_single(field), idx + 5)
+      root_fields += "\n  string {} = {};".format(format_leaf(field, "x"), idx + 5)
     elif not repeated and not leaf:
       root_fields += "\n  {} {} = {};".format(field, format_single(field), idx + 5)
 
@@ -93,9 +141,9 @@ def write_sub_messages(unique_messages, f):
 
       # generate the appropriate code to represent the field
       if leaf and repeated:
-        fields += "\n  repeated string {} = {};".format(format_repeated(field), idx + 1)
+        fields += "\n  repeated {} = {};".format(format_repeated_leaf(field, m), idx + 1)
       elif leaf and not repeated:
-        fields += "\n  string {} = {};".format(format_single(field), idx + 1)
+        fields += "\n  {} = {};".format(format_leaf(field, m), idx + 1)
       elif not leaf and repeated:
         fields += "\n  repeated {} {} = {};".format(field, format_repeated(field), idx + 1)
       elif not leaf and not repeated:
